d89ce68b3f06fc2e0c3957222de22624d1481b38
kiwiidb <kwinten.debacker@hotmail.com>
2019-10-21 15:26:36 +0200

    fix back button bug

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="blog">
     <router-view />
-    <router-link to="/" tag="a" class="back">&laquo; Back</router-link>
+    <router-link to="/blog" tag="a" class="back">&laquo; Back</router-link>
   </div>
 </template>
 <style lang="scss" scoped>
