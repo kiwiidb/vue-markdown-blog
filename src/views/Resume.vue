4fab5a5218d1f0a95d596e10660365ac9bc80e7a
kdebacker <kwinten.debacker@robovision.eu>
2019-10-22 11:16:06 +0200

    center pdf

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe style="display:block, text-align:center; margin: 0 auto" src="https://kwintendebacker.be/cv.pdf" width="65%" height="1100px"></iframe>
+    <iframe style="display:block; margin: 0 auto" src="https://kwintendebacker.be/cv.pdf" width="65%" height="1100px"></iframe>
 </template>
